424d5aa1d406347595f53771bf5d69a1255605dc
huanent <huanent@icloud.com>
2024-04-28 21:38:41 +0800

    temp

--- src/Pilotair.Web/front-app/src/workspace/header.tsx
+++ src/Pilotair.Web/front-app/src/workspace/header.tsx
@@ -1,23 +1,35 @@
 import { Dropdown, Avatar, MenuProps } from "antd"
 import { UserOutlined } from "@ant-design/icons"
+import { useNavigate } from "react-router-dom"
 
 const items: MenuProps['items'] = [
     {
-        key: '1',
+        key: 'logout',
         label: (
-            <a target="_blank" rel="noopener noreferrer" href="https://www.antgroup.com">
-                1st menu item
-            </a>
+            <a>Logout</a>
         ),
     },
 ];
 
 export function Header() {
+    const nav = useNavigate();
+    function onMenuClick({ key }: { key: string }) {
+        switch (key) {
+            case "logout":
+                nav("login", {
+                    replace: true,
+                    relative: "route"
+                })
+                break;
+
+            default:
+                break;
+        }
+    }
+
     return <>
-        <div className="flex-1">
-            
-        </div>
-        <Dropdown menu={{ items }}>
+        <div className="flex-1"></div>
+        <Dropdown menu={{ items, onClick: onMenuClick }} >
             <Avatar className="bg-slate-300" icon={<UserOutlined />} />
         </Dropdown>
     </>
